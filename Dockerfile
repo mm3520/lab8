a2c98929bae132669e22b973c49bbee905b27c2a
mm3520 <137839094+mm3520@users.noreply.github.com>
2024-03-06 07:46:39 -0600

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use the official Python image
+FROM python:3.8
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the application files to the container
+COPY . .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose the port on which the app will run
+EXPOSE 5000
+
+# Command to run the application
+CMD ["python", "app.py"]
+
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, jsonify
+import random
+import time
+import os
+
+app = Flask(__name__)
+
+# Mock Bluetooth data
+def generate_mock_data():
+    return {"timestamp": time.time(), "value": random.uniform(0, 10)}
+
+def generate_mock_data2():
+    return {"timestamp": time.time(), "value": random.uniform(0,5)}
+
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
+
+@app.route('/gamma', methods=['GET'])
+def index_gamma():
+    return render_template('index.html')
+
+@app.route('/beta', methods=['GET'])
+def index_beta():
+    return render_template('index_beta.html')
+
+@app.route('/get_gamma_data', methods=['GET'])
+def get_gamma_data():
+    mock_data = generate_mock_data()
+    return jsonify(mock_data)
+
+@app.route('/get_beta_data', methods=['GET'])
+def get_beta_data():
+    mock_data = generate_mock_data2()
+    return jsonify(mock_data)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
